4a0d12c03e61efd5ed4df3bc58af7a075cbbff6f
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-28 12:09:26 -0700

    added code to SelectionSort.h and SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "SelectionSort.h"
+
+using namespace std;
+
+void SelectionSort::selectionSort() {
+    int min;
+    int temp;
+    int i, j;
+    for(i = 0; i < 9; i++) {
+        min = i;
+        for(j = i+1; j < 10; j++) {
+            if(arr[j] < arr[min])
+                min = j;
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+void SelectionSort::displayArray() {
+    cout << "Array:\n[";
+
+    for (int i = 0; i < 9; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << arr[9] << "]\n" << endl; 
+}
---
+++ SelectionSort.h
@@ -0,0 +1,12 @@
+#ifndef SELECTIONSORT.H
+#define SELECTIONSORT.H
+
+class SelectionSort {
+
+    public:
+        int arr[10];
+        void selectionSort();
+        void displayArray();
+};
+
+#endif
