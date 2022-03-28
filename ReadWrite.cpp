12118a5ad3c75ae89d9042c16e136d79f9005c3b
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-28 14:24:10 -0700

    added ReadWrite.h and ReadWrite.cpp

---
+++ ReadWrite.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include "ReadWrite.h"
+
+using namespace std;
+
+int* ReadWrite::readFile(string fileName, int arr[10]) {
+    ifstream infile(fileName);
+    int count = 0;
+    while(!infile.fail()) {
+        infile >> arr[count++];
+    }
+
+    infile.close();
+
+    return arr;
+}
+
+void ReadWrite::writeFile(int arr[10]) {
+    ofstream outfile("SortedArray.txt");
+
+    for(int i = 0; i < 10; i++) {
+        outfile << arr[i] << endl;
+    }
+}
---
+++ ReadWrite.h
@@ -0,0 +1,13 @@
+#ifndef READWRITE_H
+#define READWRITE_H
+
+#include <string>
+
+class ReadWrite {
+
+    public:
+        int* readFile(std::string, int[10]);
+        void writeFile(int [10]);
+};
+
+#endif
